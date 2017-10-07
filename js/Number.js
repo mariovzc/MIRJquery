542aaf0da2964ebdfb29f1039eeb7582efd0e7b7
¨Mario <mario.vizcainos@gmail.com>
2017-10-07 11:24:44 -0500

    validation of repated numbers

--- js/Number.js
+++ js/Number.js
@@ -2,5 +2,23 @@ function Number(){
   const x = this;
   this.random = function(){
     return Math.floor(1000 + Math.random() * 9999);
-  }
+  };
+  this.isInValid = function (arr){
+    let inValid = false;
+    arr.forEach(function(element){
+      if((this.countInArray(arr,element)) >= 2){
+        inValid = true;
+      }
+    });    
+    return inValid;
+  };
+  this.countInArray = function (arr,number){
+    let count = 0;
+    arr.forEach(function(element){
+      if (element === number) {
+        count++;
+      }
+    });
+    return count;
+  };
 }
