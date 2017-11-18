b376113edbbc6584780e41ef2cd073808e72c00e
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-18 15:33:25 -0500

    moving all the app from jquery to vuejs

--- js/app.js
+++ js/app.js
@@ -1,55 +1,63 @@
 let random = 0
-const number = new Number()
+const numbers = new GameNumber()
 const game = new Game()
 
-let app = new Vue ({
+let app = new Vue({
   el: '#app',
-  data:{
-    text: 'Hello world'
+  data () {
+    return {
+      results: [],
+      number: '',
+      isInvalid: false,
+      scaleIn: false
+    }
+  },
+  methods: {
+    reset () {
+      this.defaultValues()
+      Materialize.toast('El numero fue reiniciado !', 3000)
+    },
+    defaultValues () {
+      random = numbers.random()
+      console.poo('Hola Tramposo -)')
+      let self = this
+      setTimeout(function () {
+        self.scaleIn = !self.scaleIn
+      }, 1000)
+      this.number = ''
+      this.values = []
+    },
+    submit () {
+      if (numbers.isInvalid(this.number.split(''))) {
+        this.isInvalid = true
+        Materialize.toast('No pueden haber caracteres repetidos', 4000)
+      } else if (this.number.length !== 4) {
+        this.isInvalid = true
+        Materialize.toast('Deben ser 4 caracteres', 4000)
+      } else {
+        $(this.number).removeClass('invalid')
+        game.validate(this.number.split(''), random.toString().split(''))
+        if (game.winner()) {
+          Materialize.toast(
+            'Tenemos un Ganador, Gracias por jugar mi amig@!!!, el juego se reiniciara automanticamente Gracias por jugar!! ',
+            4000,
+            '',
+            () => {
+              app.defaultValues()
+            }
+          )
+        } else {
+          this.results.push({ // push the value inside the ARRAY
+            last_number: this.number,
+            last_p: game.p,
+            last_f: game.f
+          })
+          this.number = ''
+        }
+      }
+    }
+  },
+  mounted () {
+    this.defaultValues()
   }
 })
-
-$(document).ready(function() {
-  $('input#number').characterCounter();
-  defaultValues();
-});
-$('form').submit(function( event ) {
-  event.preventDefault();
-  const nVal = this.number.value;
-  if(number.isInValid(nVal.split(''))){
-    $(this.number).addClass('invalid');
-    Materialize.toast('No pueden haber caracteres repetidos', 4000);
-    return;
-  }
-  if(nVal.length != 4){
-    $(this.number).addClass('invalid');
-    Materialize.toast('Deben ser 4 caracteres', 4000);
-    return;
-  }
-  $(this.number).removeClass('invalid');
-  game.validate(nVal.split(''), random.toString().split(''));
-  if(game.winner()){
-    Materialize.toast('Tenemos un Ganador, Gracias por jugar mi amig@!!!, el juego se reiniciara automanticamente Gracias por jugar!! ',
-                      4000, 
-                      '', 
-                      function(){
-                        defaultValues();
-                      });
-  }else{
-    $('table  tbody').append('<tr><td>'+ nVal +'</td><td>' + game.p() + '</td><td>' + game.f() + '</td></tr>');
-  }
-
-});
-$('#reset').click(function(){
-  defaultValues();
-  Materialize.toast('El numero fue reiniciado !', 3000);  
-});
-function defaultValues(){
-  random = number.random();
-  console.poo("Hola Tramposo ;-)");
-  setTimeout(function(){
-    $('.buttons-container').removeClass('scale-out').addClass('scale-in');
-    $('#number').focus();    
-  }, 1000);
-  $('table  tbody').html('');
-}
