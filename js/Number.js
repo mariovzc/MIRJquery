af7a00b2459b9db3a64fa9068ce25ebd6d498bf6
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-18 14:52:44 -0500

    Moving to ES6 Classes

--- js/Number.js
+++ js/Number.js
@@ -1,5 +1,30 @@
+class Number {
+  random () {
+    return Math.floor(1000 + Math.random() * 9000)
+  }
+  countInArray (arr, number) {
+    let count = 0
+    arr.forEach((element) => {
+      if (element === number) {
+        count++
+      }
+    })
+    return count
+  }
+  isInvalid (arr) {
+    let inValid = false
+    arr.forEach((element) => {
+      if ((this.countInArray(arr, element)) >= 2) {
+        inValid = true
+      }
+    })
+    return inValid
+  }
+}
+/**
 function Number(){
   const x = this;
+
   this.random = function(){
     return Math.floor(1000 + Math.random() * 9000);
   };
@@ -9,7 +34,7 @@ function Number(){
       if((this.countInArray(arr,element)) >= 2){
         inValid = true;
       }
-    });    
+    })
     return inValid;
   };
   this.countInArray = function (arr,number){
@@ -22,3 +47,4 @@ function Number(){
     return count;
   };
 }
+*/
